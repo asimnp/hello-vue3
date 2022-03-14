c531a4439e9142aa99fc881926c7577974092dda
Asim Shrestha <asimshrestha608@gmail.com>
2022-03-14 16:19:10 +0545

    create: a new vue app and mount it with template

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello Vue3')
+const template = `
+    <h1>Hello Vue3 !!!</h1>
+    <p>Welcome to my vue3 CDN page.</p>
+`
+
+const OptionsObject = { template }
+
+Vue.createApp(OptionsObject).mount('#app')
